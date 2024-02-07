c148db682ad109270da6cd70a6f93cf26ec9ec36
Mateo Arroyave <teo3851@gmail.com>
2024-02-06 19:31:49 -0500

    C2 extras

--- TrabajoGit.py
+++ TrabajoGit.py
@@ -1,7 +1,17 @@
 def git():
     nombre = input("Escriba su nombre completo: ")
     salario = 0
-    
+    valorHora = int(input("Ingresa el valor de la hora: "))
+    numeroHoras = int(input("Ingrese el numero de horas trabajadas: "))
+    extras = 0
+
+    if (numeroHoras >= 47):
+        extras = numeroHoras - 47
+
+    if (numeroHoras >= 47):
+        porcentaje = valorHora * 0.35
+        Valor_extras = porcentaje + valorHora
+        Horas_extras = extras * Valor_extras
 
 if __name__ == '__main__':
     git()
